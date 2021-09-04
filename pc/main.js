55789a2e3794d96a4e2f1bc4dea3942716482910
IzaakSantana <isaacsantana.162@gmail.com>
2021-09-03 23:26:13 -0300

    Electron first settings

---
+++ pc/main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow} = require('electron')
+const path = require('path')
+
+var teste = "Texto de teste"
+
+function createWindow () {
+    const window = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences : {
+            // path.join - junta pedaços de diretórios | __dirname - o diretório em que o script está
+            // sendo executado
+            preload: path.join(__dirname, 'preload.js') 
+        }
+    })
+
+    window.loadFile('index.html') // Não sei se precisa usar o path.join aqui, mas vamo ver no que dá.clear
+    
+}
+
+
+app.whenReady().then(() => {
+    createWindow()
+
+    // Garante que uma nova janela seja criada caso o app esteja rodando em segundo plano no MacOS
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+// Fecha o app ao fechar todas as janelas se o sistema for Linux ou Windows
+app.on('close-all-windows', () => {
+    if (process.plataform !== 'darwin') app.quit()  
+})
