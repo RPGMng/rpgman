645a3bbc73eb55e9319c3ae90558d2eecef7a010
IzaakSantana <isaacsantana.162@gmail.com>
2021-09-03 23:31:50 -0300

    Apaguei o código do preload

--- pc/preload.js
+++ pc/preload.js
@@ -1,6 +1,2 @@
 // Script chamado antes do renderer, que tem acesso tanto ao DOM quanto ao
 // ambiente Node.js
-
-var body = document.getElementsByTagName('body')[0]
-
-body.innerHTML = `<p>${teste}</p>` // pega variável do main.js
