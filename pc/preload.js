6ecad9c01d0a1ab27efb380697b50ec82ea7f6c5
IzaakSantana <isaacsantana.162@gmail.com>
2021-09-04 02:18:56 -0300

    Some little changes

--- pc/preload.js
+++ pc/preload.js
@@ -1,6 +1,16 @@
 // Script chamado antes do renderer, que tem acesso tanto ao DOM quanto ao
 // ambiente Node.js
-
+/*
 window.addEventListener('DOMContentLoaded', () => {
 
 })
+*/
+
+const { contextBridge } = require('electron')
+
+// Compartilha variáveis com o renderer
+contextBridge.exposeInMainWorld('share', {
+  //country_code = app.getLocaleCountryCode(),
+  text: "sample text"
+})
+
--- pc/renderer.js
+++ pc/renderer.js
@@ -1,13 +1,3 @@
 // Script que vai funcionar no lado web
 
-var body = document.getElementsByTagName('body')[0]
-var botao = document.querySelector('#botao')
-
-botao.addEventListener('click', texto)
-
-function texto(){
-    var par = document.createElement('p')
-    par.innerText = "Texto adicionado via código"
-    
-    body.appendChild(par)
-}
+console.log(window.share.text)
